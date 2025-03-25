743a02f6037891f557d2c1482e9ddd8f79f49ff9
John Doe <john.doe@contoso.com>
2025-03-25 12:21:08 +0530

    Your commit message

---
+++ .dist/index.js
@@ -0,0 +1,21 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() === "") return;
+
+    let li = document.createElement("li");
+    li.textContent = taskInput.value;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.className = "delete";
+    deleteBtn.onclick = function () {
+        taskList.removeChild(li);
+    };
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    
+    taskInput.value = "";
+}
